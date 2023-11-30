fc2edc924790c280fc271e2078a24fe33365c69f
Vegard Brobakken <vebr009@student.kristiania.no>
2023-11-30 14:43:04 +0100

    Revert "added implementations for http"
    This reverts commit 768632c3ccdd372802188dcc053a740578bea61e.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,11 +56,6 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
-
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
