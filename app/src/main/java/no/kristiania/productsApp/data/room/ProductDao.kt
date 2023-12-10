622c0aa8b91c353dc61cd6fbad78279517de4220
Vegard Merkesvik <veme001@student.kristiania.no>
2023-12-10 14:47:21 +0100

    Merge conflict fixed

--- app/src/main/java/no/kristiania/productsApp/data/room/ProductDao.kt
+++ app/src/main/java/no/kristiania/productsApp/data/room/ProductDao.kt
@@ -4,12 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-<<<<<<< HEAD:app/src/main/java/no/kristiania/productsApp/data/room/ProductDao.kt
 import no.kristiania.productsApp.data.Product
-=======
-import java.util.Locale.Category
->>>>>>> beb6d0059f0b697c974f9d5ffa3516eb0765bc7e:app/src/main/java/no/kristiania/productsApp/data/ProductDao.kt
-
 @Dao
 interface ProductDao {
 
