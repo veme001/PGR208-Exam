c9548713bb20bd1fdd616e7e3eaa2318ff1dd60b
Vegard Merkesvik <veme001@student.kristiania.no>
2023-12-11 23:04:27 +0100

    Added try catches for all functions in the repository

--- app/src/main/java/no/kristiania/productsApp/data/ProductRepository.kt
+++ app/src/main/java/no/kristiania/productsApp/data/ProductRepository.kt
@@ -58,48 +58,65 @@ object ProductRepository {
         }
     }
 
-        suspend fun getProductById(productId: Int): Product? {
-            return _appDatabase.getProductDao().getProductById(productId)
+    suspend fun getProductById(productId: Int): Product? {
+        return try {
+            _appDatabase.getProductDao().getProductById(productId)
+        } catch (e: Exception) {
+            Log.e("getProductById", "get product by id did not work", e)
+            null
+        }
+    }
+
+    suspend fun getProductsById(ids: List<Int>) : List<Product>{
+        return try {
+            _appDatabase.getProductDao().getProductsByIds(ids)
+        } catch (e: Exception) {
+            Log.e("getProductsById", "get products by ids did not work", e)
+            emptyList()
         }
 
-        suspend fun getProductsById(ids: List<Int>) : List<Product>{
-            return _appDatabase.getProductDao().getProductsByIds(ids)
-        }
+    }
 
-        // Categories
+    // Categories
 
-        suspend fun getProductsByCategory(category: String): List<Product> {
-            try {
-                val response = _productService.getProductsByCategory(category)
+    suspend fun getProductsByCategory(category: String): List<Product> {
+        try {
+            val response = _productService.getProductsByCategory(category)
 
-                if (response.isSuccessful) {
-                    val products = response.body() ?: emptyList()
-                    _appDatabase.getProductDao().insertProducts(products)
+            if (response.isSuccessful) {
+                val products = response.body() ?: emptyList()
+                _appDatabase.getProductDao().insertProducts(products)
 
-                    Log.d("getProductsByCategory", "Products for $category: ${products}")
-                    return _appDatabase.getProductDao().getProductsByCategory(category)
-                } else {
-                    Log.e("getProductsByCategory", "Unsuccessful response: ${response.code()}")
-                    throw Exception("could not get products by category")
-                }
-
-            } catch (e: Exception) {
-                Log.e("getProductsByCategory", "Failed to get products by category", e)
-                return emptyList()
+                Log.d("getProductsByCategory", "Products for $category: ${products}")
+                return _appDatabase.getProductDao().getProductsByCategory(category)
+            } else {
+                Log.e("getProductsByCategory", "Unsuccessful response: ${response.code()}")
+                throw Exception("could not get products by category")
             }
+
+        } catch (e: Exception) {
+            Log.e("getProductsByCategory", "Failed to get products by category", e)
+            return emptyList()
         }
+    }
 
 
-        // Shopping Cart:
-        suspend fun getShoppingCart(): List<ShoppingCartItem> {
+    // Shopping Cart:
+    suspend fun getShoppingCart(): List<ShoppingCartItem> {
+        return try {
             val itemsList = _appDatabase.getShoppingCartDao().getShoppingCart()
 
-            return itemsList.ifEmpty {
+            itemsList.ifEmpty {
                 emptyList()
             }
+        } catch (e: Exception) {
+            Log.e("getShoppingCart", "Failed to get the shoppingCart from room", e)
+            emptyList()
         }
+    }
 
-        suspend fun addShoppingCartItem(item: ShoppingCartItem) {
+    suspend fun addShoppingCartItem(item: ShoppingCartItem) {
+        try {
             val exists = _appDatabase.getShoppingCartDao().getItemById(item.productId)
 
             if (exists != null) {
@@ -107,31 +124,57 @@ object ProductRepository {
             } else {
                 _appDatabase.getShoppingCartDao().insertItem(item)
             }
+        } catch (e: Exception){
+            Log.e("addShoppingCartItem", "Failed to add item to database", e)
         }
+    }
 
-        suspend fun updateQuantity(productId: Int, quantity: Int) {
+    suspend fun updateQuantity(productId: Int, quantity: Int) {
+        try {
             _appDatabase.getShoppingCartDao().updateQuantity(productId, quantity)
+        } catch (e: Exception){
+            Log.e("updateQuantity", "Failed to update the quantity", e)
         }
 
-        suspend fun removeShoppingCartItem(item: ShoppingCartItem) {
+    }
+
+    suspend fun removeShoppingCartItem(item: ShoppingCartItem) {
+        try {
             val itemToDelete = _appDatabase.getShoppingCartDao().getItemById(item.productId)
 
             if (itemToDelete != null) {
                 _appDatabase.getShoppingCartDao().deleteItem(item)
             }
+        } catch (e: Exception){
+            Log.e("removeShoppingCartItem", "Failed to delete the item from the shopping cart", e)
         }
+    }
 
-        // Orders:
 
-        suspend fun getOrders(): List<OrderItem>{
-            return _appDatabase.getOrderDao().getAllOrders()
+    // Orders:
+    suspend fun getOrders(): List<OrderItem>{
+        return try {
+            _appDatabase.getOrderDao().getAllOrders()
+        } catch (e: Exception){
+            Log.e("getOrders","Failed to get the orders", e)
+            emptyList()
         }
+    }
 
-        suspend fun addOrder(order: OrderItem) {
+    suspend fun addOrder(order: OrderItem) {
+        try {
             return _appDatabase.getOrderDao().insertOrder(order)
+        } catch (e: Exception){
+            Log.e("addOrders","Failed to add the order", e)
         }
+    }
 
-        suspend fun getOrderById(orderId: Int): OrderItem? {
-            return _appDatabase.getOrderDao().getOrderById(orderId)
+    suspend fun getOrderById(orderId: Int): OrderItem? {
+        return try {
+            _appDatabase.getOrderDao().getOrderById(orderId)
+        } catch (e: Exception){
+            Log.e("getOrderById","Failed to get the order by id", e)
+            null
         }
+    }
 }
